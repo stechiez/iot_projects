1c856fbfa9fc2baa1f6ad209e24951be36cefad4
stechiez <39598269+stechiez@users.noreply.github.com>
2021-10-19 16:15:34 +0530

    Add files via upload

---
+++ reyax_mqtt_cloud/code/publish1/arduino_secrets.h
@@ -0,0 +1,8 @@
+#define WIFI_SSID   "xxxxxx"		//ENTER WIFI SSID HERE
+#define WIFI_PASS   "xxxxxxxxxx"	//ENTER WIFI PASSWORD HERE
+#define MQTT_BROKER "iot.reyax.com"
+#define MQTT_USER   "xxxxxxxxxx"	//ENTER REYAX USERNAME
+#define MQTT_PASS   "xxxxxxxxxx"	//ENTER REYAX PASSWORD 
+#define MQTT_CLID   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"	//ENTER DEVICE ID HERE
+#define MQTT_PORT   1883
+#define MQTT_TOPIC  "msg"
